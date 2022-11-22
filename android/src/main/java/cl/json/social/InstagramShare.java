7a5ec84cf572a3d578b1c9160cda5ca220921c6f
Zul Amri Durrin Nafis <b2b-adira@contractor.bukalapak.com>
2022-11-22 14:34:53 +0700

    Change instagram share directly post to feed

--- android/src/main/java/cl/json/social/InstagramShare.java
+++ android/src/main/java/cl/json/social/InstagramShare.java
@@ -39,7 +39,6 @@ public class InstagramShare extends SingleShareIntent {
                 return;
             }
 
-
             if (!ShareIntent.hasValidKey("type", options)) {
                 Log.e("RNShare", "No type provided");
                 return;
@@ -48,7 +47,7 @@ public class InstagramShare extends SingleShareIntent {
             String extension = this.getExtension(type);
             Boolean isImage = type.startsWith("image");
 
-            this.openInstagramIntentChooser(url, chooserTitle, isImage, extension);
+            this.shareToInstagramFeed(url, chooserTitle, isImage, extension);
     }
 
     protected void openInstagramUrlScheme(String url) {
@@ -63,39 +62,26 @@ public class InstagramShare extends SingleShareIntent {
             return ext[ext.length -1];
     }
 
-    protected void openInstagramIntentChooser(String url, String chooserTitle, Boolean isImage, String extension) {
+    protected void shareToInstagramFeed(String url, String chooserTitle, Boolean isImage, String extension) {
         Boolean shouldUseInternalStorage = ShareIntent.hasValidKey("useInternalStorage", options) && options.getBoolean("useInternalStorage");
-        ShareFile shareFile = isImage 
-            ? new ShareFile(url, "image/" + extension, "image", shouldUseInternalStorage, this.reactContext) 
-            : new ShareFile(url, "video/" + extension, "video", shouldUseInternalStorage, this.reactContext);
+        ShareFile shareFile = new ShareFile(url, "image/" + extension, "image", shouldUseInternalStorage, this.reactContext) ;
         Uri uri = shareFile.getURI();
 
-        Intent feedIntent = new Intent(Intent.ACTION_SEND);
+        Activity activity = this.reactContext.getCurrentActivity();
 
-        if (isImage) {
-            feedIntent.setType("image/*");
-        } else {
-            feedIntent.setType("video/*");
-        }
-
+        Intent feedIntent = new Intent("com.instagram.share.ADD_TO_FEED");
+        
+        feedIntent.setType("image/*");
         feedIntent.putExtra(Intent.EXTRA_STREAM, uri);
         feedIntent.setPackage(PACKAGE);
-
-        Intent storiesIntent = new Intent("com.instagram.share.ADD_TO_STORY");
-
-        storiesIntent.setDataAndType(uri, extension);
-
-        storiesIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-        storiesIntent.setPackage(PACKAGE);
-
-        Intent chooserIntent = Intent.createChooser(feedIntent, chooserTitle);
+        
+        Intent chooserIntent = Intent.createChooser(feedIntent, "Share to feed");
         chooserIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] {storiesIntent});
-
-        Activity activity = this.reactContext.getCurrentActivity();
+       
         activity.grantUriPermission(PACKAGE, uri, Intent.FLAG_GRANT_READ_URI_PERMISSION);
+      
         this.reactContext.startActivity(chooserIntent);
-        TargetChosenReceiver.sendCallback(true, true, this.getIntent().getPackage());
+         TargetChosenReceiver.sendCallback(true, true, this.getIntent().getPackage());
     }
 
     @Override
